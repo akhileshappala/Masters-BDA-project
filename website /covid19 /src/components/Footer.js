6aa899f37bffc741f45a9634e2e35e2fbebe866f
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2023-05-02 15:41:01 -0600

    complete project

---
+++ website /covid19 /src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import {memo} from 'react';
+import {useTranslation} from 'react-i18next';
+
+function Footer() {
+  const {t} = useTranslation();
+
+  return <></>;
+}
+
+export default memo(Footer);
